5ebd1d4739cf495cdcaa3f54c67e27daae333714
alyaaghania <alyaghania1@gmail.com>
2025-10-14 08:11:27 +0700

    Update web.php

--- perpusdigital_bpsjember/routes/web.php
+++ perpusdigital_bpsjember/routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\OperatorController;
+use App\Http\Controllers\PenggunaController;
 use App\Http\Controllers\KategoriController;
 
 // -- HALAMAN UTAMA --
@@ -25,3 +30,11 @@ Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
 Route::get('/operator/dashboard', [OperatorController::class, 'index'])->name('operator.dashboard');
 Route::get('/pengguna/dashboard', [PenggunaController::class, 'index'])->name('pengguna.dashboard');
+
+// CRUD kategori
+Route::get('/kategori', [KategoriController::class, 'index'])->name('kategori.kategori');
+Route::get('/kategori/tambah', [KategoriController::class, 'create'])->name('kategori.addkategori');
+Route::post('/kategori/simpan', [KategoriController::class, 'store'])->name('kategori.store');
+Route::get('/kategori/edit/{kategori}', [KategoriController::class, 'edit'])->name('kategori.editkategori');
+Route::put('/kategori/update/{kategori}', [KategoriController::class, 'update'])->name('kategori.update');
+Route::delete('/kategori/hapus/{kategori}', [KategoriController::class, 'destroy'])->name('kategori.destroy');
